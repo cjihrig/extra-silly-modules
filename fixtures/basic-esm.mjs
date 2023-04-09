98516f3daa9414fa21b260ce73ce45eb76d6dba9
cjihrig <cjihrig@gmail.com>
2023-04-08 20:00:00 -0400

    create all exports on SyntheticModules

--- fixtures/basic-esm.mjs
+++ fixtures/basic-esm.mjs
@@ -1,5 +1,12 @@
 globalThis.basic_esm ??= 0;
 globalThis.basic_esm++;
-import dep from './dep-esm.mjs';
-export default true;
 
+import fs from 'node:fs';
+import * as foo from 'node:dns';
+import { createRequire } from 'node:module';
+import dep from './dep-esm.mjs';
+
+const require = createRequire(import.meta.url);
+require('./dep-cjs');
+
+export default true;
